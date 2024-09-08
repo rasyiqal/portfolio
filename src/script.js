2001308997abfd14f77ed1f9f0dbd39fe2db4864
Rasyiqal F <rasyiqal@gmail.com>
2024-09-08 17:43:30 +0700

    portfolio v2.0
    ui update

---
+++ src/script.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', () => {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 0) {
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+    }
+});
